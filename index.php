83b5fc54055b5b35ad72890447526aa3ca635655
Yasz Avellia <zakamaragames@gmail.com>
2022-08-16 19:03:16 +0700

    sidebar test

--- index.php
+++ index.php
@@ -8,8 +8,37 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>test</h1>
-    
+<div class="sidebar d-flex flex-column p-3 bg-dark text-white h-100" style=" width: 280px;">
+        <a href="" class=" fs-4 text-decoration-none text-white">Restaurant</a>
+        <hr>
+        <ul class=" nav nav-pills flex-column mb-auto">
+            <li class=" nav-item">
+                <a href="" class=" nav-link active">Dashboard</a>
+            </li>
+            <li class=" nav-item">
+                <a href="" class=" nav-link text-white">Order</a>
+            </li>
+            <li class=" nav-item">
+                <a href="" class=" nav-link text-white">Manage Admin</a>
+            </li>
+            <li class=" nav-item">
+                <a href="" class=" nav-link text-white">Manage Menu</a>
+            </li>
+        </ul>
+    </div>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h1>yasz</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
