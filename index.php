753591663b07e18e83c17233892b5c60ada07763
Yasz Avellia <zakamaragames@gmail.com>
2022-08-16 20:26:16 +0700

    menambah icon

--- index.php
+++ index.php
@@ -12,25 +12,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tampilan</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
 <body>
 
     <div class="sidebar d-flex flex-column p-3 bg-dark text-white" style=" width: 45vh; height: 100vh; ">
     
-            <a href="" class=" h1 mb-0 fs-4 text-decoration-none text-white"><img src="prof.png" alt="">  Storage Yasz</a>
+            <a href="" class=" h1 mb-0 fs-4 text-decoration-none text-white"><img class="mb-0" src="prof.png" width="39vh" height="36vh"> Storage Yasz</a>
         <hr>
         <ul class=" nav nav-pills flex-column mb-auto">
             <li class=" nav-item">
-                <a href="" class=" nav-link active ">Dashboard</a>
+                <a href="" class=" nav-link active "><i class="bi bi-house-door"></i> Dashboard</a>
             </li>
             <li class=" nav-item">
-                <a href="" class=" nav-link text-white mt-2">Barang</a>
+                <a href="" class=" nav-link text-white mt-2"><i class="bi bi-box-seam"></i> Barang</a>
             </li>
             <li class=" nav-item">
-                <a href="" class=" nav-link text-white mt-2">Manage Admin</a>
+                <a href="" class=" nav-link text-white mt-2"><i class="bi bi-card-checklist"></i> Supplier</a>
             </li>
             <li class=" nav-item">
-                <a href="" class=" nav-link text-white mt-2">Manage Menu</a>
+                <a href="" class=" nav-link text-white mt-2"><i class="bi bi-cart"></i> Transaksi</a>
             </li>
         </ul>
     </div>
